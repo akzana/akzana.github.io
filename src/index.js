ac506a05c7e99d9e57963e0c207a6294dc9415ee
Akzana <50274932+akzana@users.noreply.github.com>
2024-12-22 11:10:40 -0800

    Update index.js
    move test log

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import { SocialProvider } from './SocialContext';
 
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 console.log("testing for clo")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <SocialProvider>
